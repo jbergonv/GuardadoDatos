a644b19d5808d7f89cff87987456510d6a6dbbb6
Jose <jbergon@iesflorenciopintado.es>
2020-10-22 11:19:11 +0200

    hola

--- app/src/main/java/com/example/guardadodatos/MainActivity.java
+++ app/src/main/java/com/example/guardadodatos/MainActivity.java
@@ -7,14 +7,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.GregorianCalendar;
 
-    Button guardar,cargar,borrar;
+public class MainActivity extends AppCompatActivity implements DialogHora.CuandoEsteSeleccionadaLaHora {
+
+    Button guardar,cargar,borrar,hora;
     EditText nombre,edad;
+    TextView textoHora;
+    Switch selector;
     static final String NOMBRE_FICHERO="DATOS";
     static final String NOMBRE = "NOMBRE";
     static final String EDAD = "EDAD";
+    static final String SWITCH = "SWITCH";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
         nombre = findViewById(R.id.editTextTextNombre);
         edad = findViewById(R.id.editTextEdad);
         borrar = findViewById(R.id.buttonBorrar);
+        selector = findViewById(R.id.switch1);
+        hora = findViewById(R.id.buttonHora);
+        textoHora = findViewById(R.id.textViewHora);
 
             guardar.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -36,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
                         SharedPreferences.Editor editor = misPreferencias.edit();
                         editor.putString(NOMBRE,nombre.getText().toString());
                         editor.putInt(EDAD,Integer.parseInt(edad.getText().toString()));
+                        editor.putBoolean(SWITCH,selector.isChecked());
                         editor.apply();
 
 
@@ -50,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
                     SharedPreferences misPreferencias = getSharedPreferences(NOMBRE_FICHERO,MODE_PRIVATE);
                     nombre.setText(misPreferencias.getString(NOMBRE,"-Sin nombre"));
                     edad.setText(""+misPreferencias.getInt(EDAD,0));
+                    selector.setChecked(misPreferencias.getBoolean(SWITCH,false));
 
 
                 }
@@ -64,6 +76,26 @@ public class MainActivity extends AppCompatActivity {
 
                 }
             });
+
+
+            hora.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    DialogHora dialogHora = new DialogHora();
+
+                    dialogHora.show(getSupportFragmentManager(),"Mi ventana de hora");
+
+
+                }
+            });
+
+    }
+
+    @Override
+    public void miSeleccion(GregorianCalendar hora) {
+
+        textoHora.setText(hora.get(GregorianCalendar.HOUR)+":"+hora.get(GregorianCalendar.MINUTE));
 
     }
 }
